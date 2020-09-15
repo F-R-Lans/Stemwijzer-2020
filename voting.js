e45f52aeb192dd791e0a04720e7407b2bf5e2d30
Floris <Floris.roderick@gmail.com>
2020-09-15 10:36:45 +0200

    html/css set up, beginning of JS

---
+++ voting.js
@@ -0,0 +1,4 @@
+//define opinion id's
+const Short = document.getElementById("Opinion_Short");
+const Long = document.getElementById("Opinion_Long");
+const Choice = document.getElementById("Results");
