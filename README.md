5c3d329540e1ca31c0c9efaf32df032c15bd6e42
Floris <Floris.roderick@gmail.com>
2020-09-22 09:50:40 +0200

    moved notes to readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 Stemwijzer 2020
+
+note on for loop 
+for (initialExpression, condition, incrementExpression) {
+    statements
+}
+initial expression defines an expression
+condition compares the initial expression, as long as the value is true, the for loop will run
+incrementExpression; add an operator to increment the initial expression
+
+
+for(let i = 0; i <5;) {
+    console.log('hello World');
+}
+i = index/loop variable
+i<5: run as long as the value of i is less than 5, a value above 5 means the condition is not met
+++ = increment operator
+statements run as long as the for loop runs
+
+dot notation is json object related 
+myObj = { "name":"John", "age":30, "car":null };
+x = myObj.name; 
+"name" is defined in the braces, and dot notation lets you access it.
+
+function() {
+
+}
+between curly brackets is the "body" and is where
+you program logic/statements, between parentheses is "inputs", referred to as "parameters"
+a parameter is a variable only active in the function
+
+a parameter is a declaration, an argument is the value 
+
+function greet(name) {
+    console.log('hello' + name):
+}
+
+greet('John')
+
+
--- voting.js
+++ voting.js
@@ -1,70 +1,58 @@
-/*note on for loop 
-for (initialExpression, condition, incrementExpression) {
-    statements
-}
-initial expression defines an expression
-condition compares the initial expression, as long as the value is true, the for loop will run
-incrementExpression; add an operator to increment the initial expression
-
-
-for(let i = 0; i <5;) {
-    console.log('hello World');
-}
-i = index/loop variable
-i<5: run as long as the value of i is less than 5, a value above 5 means the condition is not met
-++ = increment operator
-statements run as long as the for loop runs
-
-dot notation is json object related 
-myObj = { "name":"John", "age":30, "car":null };
-x = myObj.name; 
-"name" is defined in the braces, and dot notation lets you access it.
-
-*/
-
-
 //define vars
 var Question_Number = 0; //question number starts at 0 and counts up
 var Answers = []; //answers are stored in an array
 var Final_Results = [] //final results are stored in an array
-    /*for (var i = 0; i < parties.length; i++) {
-        final_results[i] = { "name": parties[i].name, "points": 0 };
-    }
-    console.log(final_results);*/
+for (var i = 0; i < parties.length; i++) {
+    final_results[i] = { "name": parties[i].name, "points": 0 };
+}
+console.log(final_results);
 
 //define opinion id's
 const Short = document.getElementById("Statement_Short");
 const Long = document.getElementById("Statement_Long");
 const Choice = document.getElementById("Results");
 
-//onclick Generates Question (onclick->start_survey->show question)
+//onclick Generates Question (onclick->start_survey->show question) (done)
 function Start_Survey() {
     ShowQuestion();
     document.getElementById("Question_Buttons").style.display = "block";
     document.getElementById("Start_Button").style.display = "none";
 }
 
-//short.inner is title, long.inner is statement
+//ShowQuestion generates questions given in data.js.short.inner is title, long.inner is statement (done)
 function ShowQuestion() {
     Short.innerHTML = subjects[Question_Number].title;
     Long.innerHTML = subjects[Question_Number].statement;
 };
 
-//AddAnswer function saves given answer in an array
-function AddAnswer(answer, Counter) {
+//AddAnswer function saves given answer in an array ()
+function AddAnswer(answer, VoteCount) {
     answers[Question_Number] = answer;
     console.log(answers);
-    NextQuestion(Counter)
+    NextQuestion(VoteCount)
+}
+
+//NextQuestion function looks if answer is given and goes to next question (done)
+function NextQuestion(counter) {
+    if (subjects[Question_Number]) {
+        if (counter == 'next' && Question_Number != subjects.length - 1) {
+            Question_Number++
+            ShowQuestion();
+        } else if (counter == 'previous' && Question_Number != 0) {
+            Question_Number--
+            ShowQuestion();
+        } else if (counter == 'next' && Question_Number != subjects.length) {
+            calc_results();
+        }
+
+    }
 }
 
 
 
-/*NextQuestion function looks if answer is given and goes to next question 
-//function NextQuestion(counter) {}
-
-//Calc_Results function calculates results, compares with party position, and adds to array if matches.button
-function Calc_Results()
+// Calc_Results function calculates results, compares with party position, and adds to array if matches.button
+// function Calc_Results()
 
 //Show_Final_Results function prints out the final results 
 
-function Show_Final_Results() */
+// function Show_Final_Results()
