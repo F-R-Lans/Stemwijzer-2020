6686a55f947d3cbe0146a6115592731e8a52af72
Floris <Floris.roderick@gmail.com>
2020-10-29 12:30:43 +0100

    added weighing page, changed routing

--- voting.js
+++ voting.js
@@ -1,15 +1,18 @@
-//define vars
+//define vars/const
 var Question_Number = 0; //question number starts at 0 and counts up
 var Answers = []; //answers are stored in an array
 var Final_Results = [] //final results are stored in an array
 for (var i = 0; i < parties.length; i++) {
     Final_Results[i] = { "name": parties[i].name, "points": 0 };
-}; // initializes Final_Results array with 0 points
+}; // initialize Final_Results array with 0 points
+const big_party = 15
+
 
 //define opinion id's
 const Short = document.getElementById("Statement_Short");
 const Long = document.getElementById("Statement_Long");
 const Choice = document.getElementById("Final_Results");
+
 
 //onclick Generates Question (onclick->start_survey->show question) (done)
 function Start_Survey() {
@@ -18,11 +21,13 @@ function Start_Survey() {
     document.getElementById("Start_Button").style.display = "none";
 };
 
+
 //ShowQuestion generates questions given in data.js.short.inner is title, long.inner is statement (done)
 function ShowQuestion() {
     Short.innerHTML = subjects[Question_Number].title;
     Long.innerHTML = subjects[Question_Number].statement;
 };
+
 
 //AddAnswer function saves given answer in an array
 function AddAnswer(Answer, VoteCount) {
@@ -46,17 +51,15 @@ function NextQuestion(counter) {
         } else if (counter == 'null' && Question_Number != subjects.length) {
             ShowQuestion();
         } else if (counter == 'next' && Question_Number != subjects.length) {
-            Calc_Results();
-
+            Weigh_Final_Results();
         }
     }
 };
 
 
-
 // Calc_Results function calculates results, compares with party position, and adds to array if matches
 //loops through questions, checks if answer matches party position, adds if matches, loops through all parties, repeats for all questions
-//.sort sorts from high(b) to low(a)
+
 function Calc_Results() {
     for (let z = 0; z < Answers.length; z++) {
         for (let x = 0; x < Final_Results.length; x++) {
@@ -68,18 +71,24 @@ function Calc_Results() {
             }
         }
     }
-    Final_Results.sort((a, b) => b.points - a.points);
+
     Show_Final_Results();
-    console.log(Final_Results);
 };
+
+function Weigh_Final_Results() {
+    document.getElementById("QuestionPage").style = "display: none";
+    document.getElementById("WeighPage").style = "display: block";
+}
 
 
 //Show_Final_Results function prints out the final results. 
+//.sort sorts from high(b) to low(a)
 //.map converts objects in array to string
-//TODO add linebreak in combo with join
+// .join makes line break and removes comma
 function Show_Final_Results() {
-    document.getElementById("QuestionPage").style = "display: none";
+    document.getElementById("WeighPage").style = "display: none";
     document.getElementById("ResultPage").style = "display: block";
+    Final_Results.sort((a, b) => b.points - a.points);
     let decide = Final_Results.map(object => `Partij: ${object.name} Punten: ${object.points}`).join("<br/>");
     Choice.innerHTML = decide;
 };
